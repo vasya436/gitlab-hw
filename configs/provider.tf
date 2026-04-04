3423c3620efb69fc5abdd6d282e30f7ee1ee268f
vasya436 <smilesins@mail.ru>
2026-04-04 04:26:23 +0400

    Update and rename crontab.cfg to provider.tf

---
+++ configs/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.100"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = "y0__xDmps27CBjB3RMgjMjYvBZbJOJ3zXjvQf-KKSRc3gzpd9-Nvw"
+  cloud_id = "b1g4davb2n5st9luj63o"
+  folder_id = "b1g87hmcnffes9cjasjl"
+}
+
+
